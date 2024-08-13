59116b9eec39c5859bcde29ced4782f0622f6d5f
httpsanuj1807 <anuj1316.be22@chitkara.edu.in>
2024-08-13 10:30:31 +0530

    updated google cb

--- server.js
+++ server.js
@@ -1290,7 +1290,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/home", // to redirect after auth
+      callbackURL: "https://freshio-two.vercel.app/auth/gogole/home", // to redirect after auth
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo", // the endpoint from where info will be collected. It is fixed, basically a API
     },
     async (accessToken, refreshToken, profile, cb) => {
